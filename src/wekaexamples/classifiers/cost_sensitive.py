7cec7208d5f562bb4a5cae4aeef8408d1ae4c5e1
Peter Reutemann <fracpete@gmail.com>
2014-10-18 07:50:18 +1300

    example for using CostSensitiveClassifier

---
+++ src/wekaexamples/classifiers/cost_sensitive.py
@@ -0,0 +1,63 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# cost_sensitive.py
+# Copyright (C) 2014 Fracpete (fracpete at gmail dot com)
+
+import os
+import weka.core.jvm as jvm
+import wekaexamples.helper as helper
+from weka.core.classes import Random
+from weka.core.converters import Loader
+from weka.classifiers import SingleClassifierEnhancer, Classifier, Evaluation
+
+
+def main():
+    """
+    Shows how to use the CostSensitiveClassifier.
+    """
+
+    # load a dataset
+    data_file = helper.get_data_dir() + os.sep + "diabetes.arff"
+    helper.print_info("Loading dataset: " + data_file)
+    loader = Loader("weka.core.converters.ArffLoader")
+    data = loader.load_file(data_file)
+    data.set_class_index(data.num_attributes() - 1)
+
+    # classifier
+    classifier = SingleClassifierEnhancer(
+        classname="weka.classifiers.meta.CostSensitiveClassifier",
+        options=["-cost-matrix", "[0 1; 2 0]", "-S", "2"])
+    base = Classifier(classname="weka.classifiers.trees.J48", options=["-C", "0.3"])
+    classifier.set_classifier(base)
+
+    folds = 10
+    evaluation = Evaluation(data)
+    evaluation.crossvalidate_model(classifier, data, folds, Random(1))
+
+
+    print("")
+    print("=== Setup ===")
+    print("Classifier: " + classifier.to_commandline())
+    print("Dataset: " + data.get_relationname())
+    print("")
+    print(evaluation.to_summary("=== " + str(folds) + " -fold Cross-Validation ==="))
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception, e:
+        print(e)
+    finally:
+        jvm.stop()
