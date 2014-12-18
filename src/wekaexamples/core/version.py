0a957c613aaa630bd7e17ed83b3321450949a4b7
Peter Reutemann <fracpete@gmail.com>
2014-12-19 09:25:44 +1300

    added example module `version` for demonstrating how to get the Weka version

---
+++ src/wekaexamples/core/version.py
@@ -0,0 +1,36 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# version.py
+# Copyright (C) 2014 Fracpete (pythonwekawrapper at gmail dot com)
+
+import weka.core.jvm as jvm
+import weka.core.version as version
+
+
+def main():
+    """
+    Just runs some example code.
+    """
+
+    print(version.weka_version())
+
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception, e:
+        print(e)
+    finally:
+        jvm.stop()
