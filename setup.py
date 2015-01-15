f70cb8fac2b50900e7e4211fc9c5b62d346652e7
Peter Reutemann <fracpete@gmail.com>
2015-01-16 10:19:40 +1300

    requiring upcoming release now

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     author='Peter "fracpete" Reutemann',
     author_email='pythonwekawrapper at gmail dot com',
     install_requires=[
-        "python-weka-wrapper>=0.2.0",
+        "python-weka-wrapper>=0.2.3",
     ],
 )
