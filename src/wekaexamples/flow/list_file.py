9bca6762bfa5e26f742df1711c3820bee33bb209
Peter Reutemann <fracpete@gmail.com>
2015-03-12 12:08:19 +1300

    simple example for listing files in a directory

---
+++ src/wekaexamples/flow/list_file.py
@@ -0,0 +1,66 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# list_files.py
+# Copyright (C) 2015 Fracpete (pythonwekawrapper at gmail dot com)
+
+import traceback
+import tempfile
+import weka.core.jvm as jvm
+from weka.flow.control import Flow
+from weka.flow.source import ListFiles
+from weka.flow.sink import Console
+
+
+def main():
+    """
+    Just runs some example code.
+    """
+
+    # setup the flow
+    flow = Flow(name="list files")
+    # flow.print_help()
+
+    listfiles = ListFiles()
+    listfiles.options["dir"] = str(tempfile.gettempdir())
+    listfiles.options["list_files"] = True
+    listfiles.options["list_dirs"] = False
+    listfiles.options["recursive"] = False
+    listfiles.options["regexp"] = ".*r.*"
+    # listfiles.print_help()
+    flow.actors.append(listfiles)
+
+    console = Console()
+    console.options["prefix"] = "Match: "
+    # console.print_help()
+    flow.actors.append(console)
+
+    # run the flow
+    msg = flow.setup()
+    if msg is None:
+        msg = flow.execute()
+        if msg is not None:
+            print("Error executing flow:\n" + msg)
+    else:
+        print("Error setting up flow:\n" + msg)
+    flow.wrapup()
+    flow.cleanup()
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception, e:
+        print(traceback.format_exc())
+    finally:
+        jvm.stop()
