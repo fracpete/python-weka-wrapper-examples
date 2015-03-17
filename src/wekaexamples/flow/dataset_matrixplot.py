fdda997b648ab3f335663bb69a020eeec9ec140a
Peter Reutemann <fracpete@gmail.com>
2015-03-18 10:04:56 +1300

    added MatrixPlot sink

---
+++ src/wekaexamples/flow/dataset_matrixplot.py
@@ -0,0 +1,71 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# dataset_matrixplot.py
+# Copyright (C) 2015 Fracpete (pythonwekawrapper at gmail dot com)
+
+import os
+import traceback
+import weka.core.jvm as jvm
+import wekaexamples.helper as helper
+from weka.flow.control import Flow
+from weka.flow.source import FileSupplier
+from weka.flow.transformer import LoadDataset
+from weka.flow.sink import MatrixPlot
+
+
+def main():
+    """
+    Just runs some example code.
+    """
+    """
+    Displays a dataset as matrixplot.
+    """
+
+    # setup the flow
+    helper.print_title("Matrix plot")
+    iris = helper.get_data_dir() + os.sep + "iris.arff"
+
+    flow = Flow(name="matrix plot")
+
+    filesupplier = FileSupplier()
+    filesupplier.options["files"] = [iris]
+    flow.actors.append(filesupplier)
+
+    loaddataset = LoadDataset()
+    flow.actors.append(loaddataset)
+
+    plot = MatrixPlot()
+    plot.options["percent"] = 50.0
+    flow.actors.append(plot)
+
+    # run the flow
+    msg = flow.setup()
+    if msg is None:
+        print("\n" + flow.tree + "\n")
+        msg = flow.execute()
+        if msg is not None:
+            print("Error executing flow:\n" + msg)
+    else:
+        print("Error setting up flow:\n" + msg)
+    flow.wrapup()
+    flow.cleanup()
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception, e:
+        print(traceback.format_exc())
+    finally:
+        jvm.stop()
