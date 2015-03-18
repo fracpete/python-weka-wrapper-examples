b9d4d8c2f6ec09921de1d7299ed5550db5d29b8b
Peter Reutemann <fracpete@gmail.com>
2015-03-18 13:14:57 +1300

    example for cross-validating clusterer

---
+++ src/wekaexamples/flow/crossvalidate_clusterer.py
@@ -0,0 +1,80 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# crossvalidate_clusterer.py
+# Copyright (C) 2015 Fracpete (pythonwekawrapper at gmail dot com)
+
+import os
+import traceback
+import weka.core.jvm as jvm
+import wekaexamples.helper as helper
+from weka.clusterers import Clusterer
+import weka.filters as filters
+from weka.flow.control import Flow
+from weka.flow.source import FileSupplier
+from weka.flow.transformer import LoadDataset, Filter, CrossValidate
+from weka.flow.sink import Console
+
+
+def main():
+    """
+    Just runs some example code.
+    """
+
+    # setup the flow
+    helper.print_title("Cross-validate clusterer")
+    iris = helper.get_data_dir() + os.sep + "iris.arff"
+
+    flow = Flow(name="cross-validate clusterer")
+
+    filesupplier = FileSupplier()
+    filesupplier.options["files"] = [iris]
+    flow.actors.append(filesupplier)
+
+    loaddataset = LoadDataset()
+    flow.actors.append(loaddataset)
+
+    flter = Filter()
+    flter.name = "Remove class"
+    flter.options["filter"] = filters.Filter(
+        classname="weka.filters.unsupervised.attribute.Remove", options=["-R", "last"])
+    flow.actors.append(flter)
+
+    cv = CrossValidate()
+    cv.options["setup"] = Clusterer(classname="weka.clusterers.EM")
+    flow.actors.append(cv)
+
+    console = Console()
+    console.options["prefix"] = "Loglikelihood: "
+    flow.actors.append(console)
+
+    # run the flow
+    msg = flow.setup()
+    if msg is None:
+        print("\n" + flow.tree + "\n")
+        msg = flow.execute()
+        if msg is not None:
+            print("Error executing flow:\n" + msg)
+    else:
+        print("Error setting up flow:\n" + msg)
+    flow.wrapup()
+    flow.cleanup()
+
+if __name__ == "__main__":
+    try:
+        jvm.start()
+        main()
+    except Exception, e:
+        print(traceback.format_exc())
+    finally:
+        jvm.stop()
