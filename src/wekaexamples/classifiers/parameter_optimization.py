ad82cfb97d6b5a11d19837f1f41be920f2a78e11
Peter Reutemann <fracpete@gmail.com>
2015-04-20 15:50:08 +1200

    added MultiSearch meta-classifier with convenience properties

--- src/wekaexamples/classifiers/parameter_optimization.py
+++ src/wekaexamples/classifiers/parameter_optimization.py
@@ -20,7 +20,8 @@ import traceback
 import weka.core.jvm as jvm
 import wekaexamples.helper as helper
 from weka.core.converters import Loader
-from weka.classifiers import Classifier, GridSearch
+from weka.core.classes import ListParameter, MathParameter
+from weka.classifiers import Classifier, GridSearch, MultiSearch
 
 
 def gridsearch():
@@ -28,6 +29,8 @@ def gridsearch():
     Applies GridSearch to a dataset. GridSearch package must be not be installed, as the monolithic weka.jar
     already contains this package.
     """
+
+    helper.print_title("GridSearch")
 
     # load a dataset
     fname = helper.get_data_dir() + os.sep + "bolts.arff"
@@ -50,16 +53,56 @@ def gridsearch():
     print("\nBest setup:\n" + grid.best.to_commandline())
 
 
+def multisearch():
+    """
+    Applies MultiSearch to a dataset. "multisearch-weka-package" package must be installed.
+    """
+
+    helper.print_title("MultiSearch")
+
+    # load a dataset
+    fname = helper.get_data_dir() + os.sep + "bolts.arff"
+    helper.print_info("Loading train: " + fname)
+    loader = Loader(classname="weka.core.converters.ArffLoader")
+    train = loader.load_file(fname)
+    train.class_is_last()
+
+    # classifier
+    multi = MultiSearch(
+        options=["-sample-size", "100.0", "-initial-folds", "2", "-subsequent-folds", "2",
+                 "-num-slots", "1", "-S", "1"])
+    multi.evaluation = "CC"
+    mparam = MathParameter()
+    mparam.prop = "classifier.kernel.gamma"
+    mparam.minimum = -3.0
+    mparam.maximum = 3.0
+    mparam.step = 1.0
+    mparam.base = 10.0
+    mparam.expression = "pow(BASE,I)"
+    lparam = ListParameter()
+    lparam.prop = "classifier.C"
+    lparam.values = ["-2.0", "-1.0", "0.0", "1.0", "2.0"]
+    multi.parameters = [mparam, lparam]
+    cls = Classifier(
+        classname="weka.classifiers.functions.SMOreg",
+        options=["-K", "weka.classifiers.functions.supportVector.RBFKernel"])
+    multi.classifier = cls
+    multi.build_classifier(train)
+    print("Model:\n" + str(multi))
+    print("\nBest setup:\n" + multi.best.to_commandline())
+
+
 def main():
     """
     Calls the parameter optimization method(s).
     """
-    gridsearch()
+    #gridsearch()
+    multisearch()
 
 
 if __name__ == "__main__":
     try:
-        jvm.start()
+        jvm.start(packages=True)
         main()
     except Exception, e:
         print(traceback.format_exc())
