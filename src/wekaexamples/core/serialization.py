571a0de34de2c2f557ddf6edbbee2cdceaf932dc
Peter Reutemann <fracpete@gmail.com>
2019-11-10 18:29:18 +1300

    making use of new serialize/deserialize methods of Classifier class

--- src/wekaexamples/core/serialization.py
+++ src/wekaexamples/core/serialization.py
@@ -12,7 +12,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # serialization.py
-# Copyright (C) 2014 Fracpete (pythonwekawrapper at gmail dot com)
+# Copyright (C) 2014-2019 Fracpete (pythonwekawrapper at gmail dot com)
 
 import os
 import tempfile
@@ -43,14 +43,14 @@ def main():
     classifier.build_classifier(iris_data)
 
     # save and read object
-    helper.print_title("I/O: single object")
+    helper.print_title("I/O: model (using serialization module)")
     outfile = tempfile.gettempdir() + os.sep + "j48.model"
     serialization.write(outfile, classifier)
     model = Classifier(jobject=serialization.read(outfile))
     print(model)
 
     # save classifier and dataset header (multiple objects)
-    helper.print_title("I/O: single object")
+    helper.print_title("I/O: model and header (using serialization module)")
     serialization.write_all(outfile, [classifier, Instances.template_instances(iris_data)])
     objects = serialization.read_all(outfile)
     for i, obj in enumerate(objects):
@@ -60,6 +60,21 @@ def main():
         elif javabridge.get_env().is_instance_of(obj, javabridge.get_env().find_class("weka/classifiers/Classifier")):
             obj = Classifier(jobject=obj)
         print(obj)
+
+    # save and read object
+    helper.print_title("I/O: just model (using Classifier class)")
+    outfile = tempfile.gettempdir() + os.sep + "j48.model"
+    classifier.serialize(outfile)
+    model, _ = Classifier.deserialize(outfile)
+    print(model)
+
+    # save classifier and dataset header (multiple objects)
+    helper.print_title("I/O: model and header (using Classifier class)")
+    classifier.serialize(outfile, header=iris_data)
+    model, header = Classifier.deserialize(outfile)
+    print(model)
+    if header is not None:
+        print(header)
 
 
 if __name__ == "__main__":
